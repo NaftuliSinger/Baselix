6701b8b61782837a4b9d64ded3c44ead75cfc536
NaftuliSinger <naftulisinger@gmail.com>
2026-03-31 13:18:30 +0100

    Enhance ApiError function to include status code in the error response

--- internal/utils/api.go
+++ internal/utils/api.go
@@ -3,5 +3,5 @@ package utils
 import "github.com/gin-gonic/gin"
 
 func ApiError(c *gin.Context, statusCode int, message string) {
-	c.JSON(statusCode, gin.H{"error": message})
+	c.JSON(statusCode, gin.H{"error": statusCode, "message": message})
 }
